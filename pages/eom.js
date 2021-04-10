2713bdce04920010e3295f6e4ad36dfa92daa2aa
Babagana Abba <babagabba@rocketmail.com>
2021-04-11 00:10:10 +0100

    added header to eom

--- pages/eom.js
+++ pages/eom.js
@@ -1,21 +1,40 @@
+import Head from 'next/head/';
 import Toolbar from '../components/Toolbar';
 import styles from '../styles/EOM.module.css';
 
 const EOM = () => {
+    const image = "https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bGFuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80";
     return (
-        <div className="page-container">
-            <Toolbar />
-            <div className={styles.main}>
-                <h1>Employee of the Month</h1>
+        <>
+            <Head>
+                <title>Employee Of The Month</title>
+                <meta
+                    name="description"
+                    content="This month's employee of the month is Babagana Abba"
+                />
 
-                <div className={styles.employeeOfTheMonth}>
-                    <h3>Babagana Abba</h3>
-                    <h6>Web Developer</h6>
-                    <img src="https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bGFuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
-                    <p>Just a simple web developer guy</p>
+                <meta property="og:image" content={image} />
+                <meta property="og:title" content="Employee Of The Month" />
+                <meta property="og:description" content="This month's employee of the month is Babagana Abba" />
+
+                <meta property="twitter:image" content={image} />
+                <meta property="twitter:title" content="Employee Of The Month" />
+                <meta property="twitter:description" content="This month's employee of the month is Babagana Abba" />
+            </Head>
+            <div className="page-container">
+                <Toolbar />
+                <div className={styles.main}>
+                    <h1>Employee of the Month</h1>
+
+                    <div className={styles.employeeOfTheMonth}>
+                        <h3>Babagana Abba</h3>
+                        <h6>Web Developer</h6>
+                        <img src={image} alt=""/>
+                        <p>Just a simple web developer guy</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
